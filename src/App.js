b02c1d01f04bd462101bd19239bacd104a95fbb1
Mukesh Aryal <131770762+mukesharyal@users.noreply.github.com>
2025-03-09 18:27:49 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,51 +1,40 @@
 import React from "react";
 import Card from "./components/Card";
 import Filter from "./components/Filter";
-import "./App.css"
+import "./App.css";
+// Import the local JSON data
+import jobData from "./data.json";
 
-export default function App()
-{
+export default function App() {
     let [wholeData, setWholeData] = React.useState([]);
     
     const [data, setData] = React.useState([]);
-
     const [filters, setFilters] = React.useState([]);
 
     React.useEffect(() => {
-        fetch("https://storage.googleapis.com/programiz-static/hiring/software/job-listing-page-challenge/data.json")
-        .then((result) => result.json())
-        .then((result) => {
-            setWholeData(result);
-            setData(result);
-        });
+        // Set the data from the local JSON file
+        setWholeData(jobData);
+        setData(jobData);
     }, []);
 
     React.useEffect(() => {
         const arr = [];
-
-        for(let i = 0; i < wholeData.length; i++)
-        {
-            for(let j = 0; j < filters.length; j++)
-            {
-                if(wholeData[i].keywords.includes(filters[j]))
-                {
-                    if(!arr.includes(wholeData[i]))
-                    {
+        for(let i = 0; i < wholeData.length; i++) {
+            for(let j = 0; j < filters.length; j++) {
+                if(wholeData[i].keywords.includes(filters[j])) {
+                    if(!arr.includes(wholeData[i])) {
                         arr.push(wholeData[i]);
                     }
                 }
             }
         }
-        if(arr.length === 0)
-        {
+        if(arr.length === 0) {
             setData(wholeData);
         }
-        else
-        {
+        else {
             setData(arr);
         }
     }, [filters, wholeData]);
-
     
     return(
         <>
@@ -54,15 +43,14 @@ export default function App()
                 setFilters={setFilters}
             />
             {data.map(
-                        (item) => 
-                        <Card
-                            key={item.posted_on}
-                            content={item}
-                            filters={filters}
-                            setFilters={setFilters}
-                        />
-                    )
-            }
+                (item) => 
+                <Card
+                    key={item.posted_on}
+                    content={item}
+                    filters={filters}
+                    setFilters={setFilters}
+                />
+            )}
         </>
     );
 }
